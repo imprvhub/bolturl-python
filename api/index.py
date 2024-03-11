c983e2e04c6495c17497d94d7bd6819a7049b398
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-03-11 17:10:53 -0300

    feat: mysql migration - to postgres 'supabase'

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 import os
-import pymysql
+import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from hashids import Hashids
@@ -8,69 +8,81 @@ from werkzeug.exceptions import InternalServerError
 load_dotenv()
 
 app = Flask(__name__, static_folder='static')
-domain_url = os.getenv("DOMAIN_URL", "https://bolturl.site") 
+domain_url = os.getenv("DOMAIN_URL", "https://bolturl.site")
 hashids_salt = os.getenv("HASHIDS_SALT")
-hashids = Hashids(salt=hashids_salt, min_length=4)  
-connection = pymysql.connect(
-        host=os.getenv("DATABASE_HOST"),
-        user=os.getenv("DATABASE_USERNAME"),
-        passwd=os.getenv("DATABASE_PASSWORD"),
-        db=os.getenv("DATABASE"),
-        ssl={"ssl_accept": "strict"}
-    )
+hashids = Hashids(salt=hashids_salt, min_length=4)
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_SSLMODE = os.getenv("DB_SSLMODE")
 
-with connection.cursor() as cursor:
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS urls (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                original_url VARCHAR(255) NOT NULL,
-                short_url VARCHAR(255) NOT NULL
-            )
-        """)
+connection = psycopg2.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    dbname=DB_NAME,
+    sslmode=DB_SSLMODE
+)
+
+connection.autocommit = True
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS urls (
+        id SERIAL PRIMARY KEY,
+        original_url VARCHAR(255) NOT NULL,
+        short_url VARCHAR(255) NOT NULL
+    )
+""")
+
 def is_valid(url):
     url = url.lower()
     if not url.startswith(('http://', 'https://')):
         url = 'https://' + url
     return url
-def proccess_input(original_url):
-    with connection.cursor() as cursor:
-        validated_url = is_valid(original_url)
-        cursor.execute("INSERT INTO urls (original_url, short_url) VALUES (%s, %s)", (validated_url, ''))
-        url_id = cursor.lastrowid
-        url_code = hashids.encode(url_id)
-        complete_url = f"{domain_url}/{url_code}"
-        cursor.execute("UPDATE urls SET short_url = %s WHERE id = %s", (complete_url, url_id))
-        return complete_url
-    
+
+def process_input(original_url):
+    validated_url = is_valid(original_url)
+    cursor.execute("INSERT INTO urls (original_url, short_url) VALUES (%s, %s) RETURNING id", (validated_url, ''))
+    url_id = cursor.fetchone()[0]
+    url_code = hashids.encode(url_id)
+    complete_url = f"{domain_url}/{url_code}"
+    cursor.execute("UPDATE urls SET short_url = %s WHERE id = %s", (complete_url, url_id))
+    return complete_url
+
 @app.route('/')
 def index():
     return render_template('index.html', bolted_url=None)
+
 @app.route('/bolted', methods=['POST'])
-def acortar():
+def shorten():
     original_url = request.form['url']
-    bolted_url = proccess_input(original_url)
+    bolted_url = process_input(original_url)
     return render_template('index.html', bolted_url=bolted_url)
 
 @app.route('/<url_code>')
-def redirigir(url_code):
+def redirect(url_code):
     try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT original_url FROM urls WHERE short_url = %s", (f"{domain_url}/{url_code}",))
-            result = cursor.fetchone()
+        cursor.execute("SELECT original_url FROM urls WHERE short_url = %s", (f"{domain_url}/{url_code}",))
+        result = cursor.fetchone()
 
-            if result:
-                original_url = result[0]
-                return render_template('redirect.html', original_url=original_url)
-            else:
-                return "URL no encontrada"
-    except InternalServerError:
-        # Manejar la excepción aquí
+        if result:
+            original_url = result[0]
+            return render_template('redirect.html', original_url=original_url)
+        else:
+            return "URL no encontrada"
+    except psycopg2.Error as e:
+        error_message = f"Error de PostgreSQL: {e}"
+        app.logger.error(error_message)
         return "Error interno del servidor"
-    
+
+
 @app.route('/user_agreements')
 def user_agreements():
     return render_template('user_agreements.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
